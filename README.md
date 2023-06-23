f68fac73354fe773296209451acc25e53ba4e0ad
deshauni <98121733+deshauni@users.noreply.github.com>
2023-06-23 09:57:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular-14-Custom-Pipes
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-component-z1y37s)
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+     
+    ]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
